7f62f9b2326bc58eca07c7c99e8a3f205dc2e2a9
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-02 18:36:27 +0100

    added the table of content

---
+++ table.of.content.ts
@@ -0,0 +1,12 @@
+export const tableOfContents = `
+Docs: https://www.typescriptlang.org/docs/handbook/utility-types.html 
+
+1. Omit
+2. Pick
+3. Partial
+4. Required
+5. Record
+6. Parameter
+7. ReturnType
+
+`
