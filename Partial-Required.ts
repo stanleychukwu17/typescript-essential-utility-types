79508439ad9ff0c2b8991d5f74c0bdbaa3ad87ca
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-02 19:16:52 +0100

    Finished an example on Partial and Required Utilities

---
+++ Partial-Required.ts
@@ -0,0 +1,19 @@
+import { User } from "./Users";
+
+/**
+Partial<Type>
+Constructs a type with all properties of Type set to optional
+*/
+
+type UserPartial = Partial<User>;
+// this is mainly used when tying the props of an input where almost all the fields are optional
+// if we wanted the name of the input to be mandatory, we could do:
+type UserPartial_1 = Partial<User> & { name: string }
+
+/**
+Partial<Type>
+Constructs a type with all properties of Type set to Required, the opposite of Partial
+*/
+
+type UserRequired = Required<User>; // we can also do:
+type UserRequired_1 = Required<User> & { city?: string} // all options will be required except for city
