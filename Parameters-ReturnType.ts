f474298e4ca05f3c682e4d953ff3b77035d7e87d
stanleychukwu17 <stanleychukwu17@yahoo.com>
2022-08-18 22:54:23 +0100

    minor final changes

--- Parameters-ReturnType.ts
+++ Parameters-ReturnType.ts
@@ -2,7 +2,7 @@ import { User } from "./users";
 
 /**
 Parameters<Type>
-Constructs a tuple type from the types used in the parameters of a function type Type
+Constructs a tuple type from the types used in the parameters of a function
 */
 function createUser(username: string, password: string) {
     return {username, password}
@@ -21,8 +21,9 @@ createUser(...data) // destructure the array and leaves u with something like cr
 
 /**
 So the teacher showed another example using an object
-hover on all answers below to see their results, but the answer i want is ('createUserInput_2' or 'createUserInput_3') they are both the same but 'createUserInput_3' looks more typescript-eee.
-the result of CreateUserInput_2 and CreateUserInput_3 and the same. but remember that anytime you use number, a looping across all values of the array or object will occur
+hover on all answers below to see their results, but the answer i want is ('createUserInput_2' or 'createUserInput_3') they are both the same but 'createUserInput_3' looks more
+typescript-eee. the result of CreateUserInput_2 and CreateUserInput_3 are the same. but remember that anytime you use number, a looping across all values of the array or object
+will occur
 
 But the explanation of the answer below is, if the argument of the function is an object and we wanted the 'Parameters' type to be an object, just attach '[]' at the end when you
 call 'Parameters'.
@@ -42,13 +43,14 @@ createUser_1(data_2)
 
 /**
 ReturnType<Type>
-Constructs a type consisting of the return type of function Type, i.e based on, as usual hover over both answers to see their results
+Constructs a type consisting of the return type of function Type, i.e based on whatever are the types of the values returned from the function.
+as usual hover over both answers to see their results
 */
 
 type CreateUserResult = ReturnType<typeof createUser>
 type CreateUserResult_1 = ReturnType<typeof createUser_1>
 
-// if you have an sync function, do the below, CreateUserResult_3 is the right answer, but over over both answers to see the difference
+// if you have an sync function, do the below, CreateUserResult_3 is the right answer, but hover over both answers to see the difference
 async function createUser_2 ({username, password}: {username: string, password: string}) {
     return {username, password, secret:true}
 }
