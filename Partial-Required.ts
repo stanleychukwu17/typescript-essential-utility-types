868deba5ad8d04a48ba29a961ae689503f3b1f1d
stanley chukwu <stanleychukwu17@yahoo.com>
2022-11-02 08:35:39 +0100

    more examples on the partial and required functions

--- Partial-Required.ts
+++ Partial-Required.ts
@@ -9,6 +9,11 @@ type UserPartial = Partial<User>;
 // this is mainly used when typing the props of an input where all the fields are optional
 // if we wanted the name of the input to be mandatory, we could do:
 type UserPartial_1 = Partial<User> & { name: string }
+// while doing some revision, same idea just dropped in now.. we can archive the same result as above like below:
+type u1 = Partial<User>;
+type u2 = Pick<User, 'name'|'password'>
+type uFinal = u1 & u2
+// const james: uFinal = {} // un-comment and see the result for yo self!
 
 /**
 Partial<Type>
